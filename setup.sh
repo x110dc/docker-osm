1fdd75277cfba1427a92bc80cb7e84042af394a3
Daniel Craigmile <dcraigmile@texastribune.org>
2015-03-12 16:27:29 -0500

    open to all and create osm db

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 CONF="/etc/postgresql/9.3/main/postgresql.conf"
 
 # Restrict subnet to docker private network
-echo "host    all             all             172.17.0.0/16               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+echo "host    all             all             0.0.0.0/0               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
 # Listen on all ip addresses
 echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
 echo "port = 5432" >> /etc/postgresql/9.3/main/postgresql.conf
@@ -13,8 +13,8 @@ echo "port = 5432" >> /etc/postgresql/9.3/main/postgresql.conf
 
 echo "ssl = true" >> $CONF
 #echo "ssl_ciphers = 'DEFAULT:!LOW:!EXP:!MD5:@STRENGTH' " >> $CONF
-#echo "ssl_renegotiation_limit = 512MB "  >> $CONF 
-echo "ssl_cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem'" >> $CONF 
-echo "ssl_key_file = '/etc/ssl/private/ssl-cert-snakeoil.key'" >> $CONF 
-#echo "ssl_ca_file = ''                       # (change requires restart)" >> $CONF 
-#echo "ssl_crl_file = ''" >> $CONF 
+#echo "ssl_renegotiation_limit = 512MB "  >> $CONF
+echo "ssl_cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem'" >> $CONF
+echo "ssl_key_file = '/etc/ssl/private/ssl-cert-snakeoil.key'" >> $CONF
+#echo "ssl_ca_file = ''                       # (change requires restart)" >> $CONF
+#echo "ssl_crl_file = ''" >> $CONF
